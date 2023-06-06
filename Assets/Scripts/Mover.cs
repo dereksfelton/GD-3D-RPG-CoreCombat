24bc9c25b3a70ee0841f0d8656e3b6a9aaf2b728
dereksfelton <derekfelton@gmail.com>
2023-06-06 14:44:12 -0400

    After 2.12

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,6 +4,9 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
+
+    Ray lastRay;
+
     private GameObject player;
 
     private void Awake()
@@ -13,6 +16,12 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
         player.GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
