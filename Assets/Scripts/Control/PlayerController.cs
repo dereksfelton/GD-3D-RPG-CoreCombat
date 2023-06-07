32304b6174480e4bd358f902c311539c658f10f4
dereksfelton <derekfelton@gmail.com>
2023-06-07 12:21:40 -0400

    Before 04.32 - Implemented every-frame raycast for both movement and combat

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,56 +1,62 @@
 using RPG.Combat;
 using RPG.Movement;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 namespace RPG.Control
 {
-    public class PlayerController : MonoBehaviour
-    {
-        void Update()
-        {
-            InteractWithCombat();
-            InteractWithMovement();
-        }
+   public class PlayerController : MonoBehaviour
+   {
+      void Update()
+      {
+         if (InteractWithCombat()) return;
+         if (InteractWithMovement()) return;
+         print("Nothing to do...");
+      }
 
-        private void InteractWithCombat()
-        {
-            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
-            foreach (RaycastHit hit in hits)
+      private bool InteractWithCombat()
+      {
+         RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+         foreach (RaycastHit hit in hits)
+         {
+            // see if this hit is a CombatTarget
+            CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+            if (target == null) continue;
+
+            // if so, and if we clicked the left mouse button...
+            if (Input.GetMouseButtonDown(0))
             {
-                // see if this hit is a CombatTarget
-                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-                if (target == null) continue;
-
-                // if so, and if we clicked the left mouse button...
-                if (Input.GetMouseButtonDown(0))
-                {
-                    GetComponent<Fighter>().Attack(target);
-                }
+               GetComponent<Fighter>().Attack(target);
             }
-        }
 
-        private void InteractWithMovement()
-        {
+            // return that we DID interact with combat,
+            // even if we just hovered over a combat target
+            return true;
+         }
+         return false;
+      }
+
+      private bool InteractWithMovement()
+      {
+         RaycastHit hit;
+         bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+
+         if (hasHit)
+         {
             if (Input.GetMouseButton(0))
             {
-                MoveToCursor();
+               GetComponent<Mover>().MoveTo(hit.point);
             }
-        }
+            // return that we DID interact with movement,
+            // even if we just hovered over a movement target
+            return true;
+         }
+         return false;
+      }
 
-        private void MoveToCursor()
-        {
-            RaycastHit hit;
-            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
-
-            if (hasHit)
-            {
-                GetComponent<Mover>().MoveTo(hit.point);
-            }
-        }
-
-        private static Ray GetMouseRay()
-        {
-            return Camera.main.ScreenPointToRay(Input.mousePosition);
-        }
-    }
+      private static Ray GetMouseRay()
+      {
+         return Camera.main.ScreenPointToRay(Input.mousePosition);
+      }
+   }
 }
