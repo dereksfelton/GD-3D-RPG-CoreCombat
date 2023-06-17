e34d4d0f1b7198f4a16ada31e3fad7646eadf018
Derek S. Felton <derekfelton@gmail.com>
2023-06-17 17:25:27 -0400

    Post 10-129: Added Unarmed as a weapon scriptable object

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -6,9 +6,7 @@ namespace RPG.Combat
 {
    public class Fighter : MonoBehaviour, IAction
    {
-      [SerializeField] float weaponRange = 2f;
       [SerializeField] float timeBetweenAttacks = 1f;
-      [SerializeField] float weaponDamage = 5f;
       [SerializeField] Transform handTransform = null;
       [SerializeField] Weapon weapon = null;
 
@@ -67,12 +65,12 @@ namespace RPG.Combat
       void Hit()
       {
          if (target == null) return;
-         target.TakeDamage(weaponDamage);
+         target.TakeDamage(weapon.Damage);
       }
 
       private bool TargetIsInRange()
       {
-         return Vector3.Distance(transform.position, target.transform.position) <= weaponRange;
+         return Vector3.Distance(transform.position, target.transform.position) <= weapon.Range;
       }
 
       public bool CanAttack(GameObject combatTarget)
--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -5,13 +5,26 @@ namespace RPG.Combat
    [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Make New Weapon", order = 0)]
    public class Weapon : ScriptableObject
    {
-      [SerializeField] GameObject weaponPrefab = null;
+      [SerializeField] GameObject equippedPrefab = null;
       [SerializeField] AnimatorOverrideController animatorOverride = null;
-      
+
+      [SerializeField] float weaponRange = 0f;
+      [SerializeField] float weaponDamage = 0f;
+
+      public float Range { get { return weaponRange; } }
+      public float Damage { get { return weaponDamage; } }
+
+
       public void Spawn(Transform handTransform, Animator animator)
       {
-         Instantiate(weaponPrefab, handTransform);
-         animator.runtimeAnimatorController = animatorOverride;
+         if (equippedPrefab != null)
+         {
+            Instantiate(equippedPrefab, handTransform);
+         }
+         if (animatorOverride != null)
+         {
+            animator.runtimeAnimatorController = animatorOverride;
+         }
       }
    }
 }
