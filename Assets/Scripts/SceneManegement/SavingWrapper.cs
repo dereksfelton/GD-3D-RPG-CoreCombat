7250f3b9d1f92f0ab93871b515d91213c4c024b8
Derek S. Felton <derekfelton@gmail.com>
2023-06-27 15:39:43 -0400

    Post 11-167: Fix onload race cond. which could prevent good lvl update

--- Assets/Scripts/SceneManegement/SavingWrapper.cs
+++ Assets/Scripts/SceneManegement/SavingWrapper.cs
@@ -10,7 +10,12 @@ namespace RPG.SceneManagement
 
       [SerializeField] float fadeInDuration = 0.2f;
 
-      private IEnumerator Start()
+      private void Awake()
+      {
+         StartCoroutine(LoadLastScene());
+      }
+
+      private IEnumerator LoadLastScene()
       {
          Fader fader = FindFirstObjectByType<Fader>();
          fader.FadeOutImmediate();
